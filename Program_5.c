db99ac0bf5782c17a6c604d2981b0632a2b84cb1
spoorthi69 <20cse0094@gmail.com>
2021-02-18 19:31:16 +0000

    To find max and min numbers using array concept.

--- Program_5.c
+++ Program_5.c
@@ -5,13 +5,28 @@
 
 int main()
 {
-    int arr[10], n, i;
+    int arr[3], n, i,max,min;
     scanf("%d", &n);
-
     for(i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+    scanf("%d", &arr[i]);
+  
+    max = arr[0];
+    min = arr[0];
+    
 
     //Write your code here
-  
+      for(i=1;i<n;i++)
+      {
+        if(arr[i]>max)
+        max= arr[i];
+      
+      
+        if(arr[i]<min)
+        min= arr[i];
+      }
+       printf("max=%d\n",max);
+       printf("min=%d\n",min); 
+    
+      
     return 0;
 }
